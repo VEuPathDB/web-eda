496ae2e57394b4ecf324ae772f939b0e7926b7c9
Sam Kendrick <skendri2@upenn.edu>
2023-03-06 14:22:05 -0600

    refactor: replace iife with ternary

--- src/lib/map/analysis/MapAnalysis.tsx
+++ src/lib/map/analysis/MapAnalysis.tsx
@@ -248,11 +248,8 @@ export function MapAnalysisImpl(props: Props & CompleteAppState) {
     studyMetadata.rootEntity.id,
   ]);
 
-  const totalEntitiesInSampleCount = (() => {
-    if (!totalCounts.value || !outputEntity) return 0;
-
-    return totalCounts.value[outputEntity.id];
-  })();
+  const outputEntityTotalCount =
+    totalCounts.value && outputEntity ? totalCounts.value[outputEntity.id] : 0;
 
   const fullScreenActions = (
     <>
@@ -408,7 +405,7 @@ export function MapAnalysisImpl(props: Props & CompleteAppState) {
                 onAnalysisNameEdit={analysisState.setName}
                 onToggleExpand={() => setMapHeaderIsExpanded((c) => !c)}
                 studyName={studyRecord.displayName}
-                totalEntityCount={totalEntitiesInSampleCount}
+                totalEntityCount={outputEntityTotalCount}
                 totalEntityInSubsetCount={totalEntityCount}
                 visibleEntityCount={
                   totalVisibleWithOverlayEntityCount ?? totalVisibleEntityCount
